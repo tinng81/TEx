992f88252f328b58034026305ca081a53b0e1a62
tinng81 <tinng801@gmail.com>
2019-07-04 23:50:15 +0700

    Step 6
    Display Keypress with ASCII code. Check if Control characters (E.g. Ctrl-A) first

--- tex.c
+++ tex.c
@@ -13,7 +13,15 @@ int main(int argc, char const *argv[])
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if (iscntrl(c))
+        {
+            printf("%d\n", c);
+        }
+        else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
 
     return 0;
 }
