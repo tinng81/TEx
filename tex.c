c5fc6ec86e7fe5ad9dcf020c72e09d723c4c4d02
tinng81 <tinng801@gmail.com>
2019-07-05 03:02:08 +0700

    :ballot_box_with_check: Step 14 Refactor code
    Note: turned off printf input
    
    Refactor main func into input handler: ReadKey & ProcessKey

--- tex.c
+++ tex.c
@@ -1,3 +1,7 @@
+/**
+ * @brief File Headers
+ * @details Terminal, I/O, Error
+ */
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,41 +9,43 @@
 #include <unistd.h>
 #include <errno.h>
 
+/**
+ * @brief Define Keys
+ * @details <Ctrl-k>
+*/
+#define CTRL_KEY(k) ( k & 0x1f )
+
 struct termios orig_termios;
 
+/**
+ * @brief Function Prototypes
+ */
 void enableRawMode();
 void disableRawMode();
 void terminate(const char *);
+char texReadKey();
+void texProcessKey();
 
+/**
+ * @brief main
+ * @details int main
+ */
 int main(int argc, char const *argv[])
 {
 
     enableRawMode();
 
     while(1){
-        char c = "\0";
-        // read(STDIN_FILENO, &c, 1) == 1;
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) // Cygwin compatibility
-        {
-            terminate("read");
-        }        
-
-        if (iscntrl(c))
-        {
-            printf("%d\r\n", c);
-        }
-        else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q')
-        {
-            break;
-        }
+        texProcessKey();
     }
 
     return 0;
 }
 
+/**
+ * @brief Terminal API
+ * @details Enable Raw Input
+ */
 void enableRawMode() {
     // tcgetattr(STDIN_FILENO, &orig_termios);
     if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
@@ -55,8 +61,8 @@ void enableRawMode() {
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     raw.c_cflag |= (CS8);
 
-    raw.c_cc[VMIN] = 0;
-    raw.c_cc[VTIME] = 10; // 10 * 100 msec = 10s timeout
+    // raw.c_cc[VMIN] = 0;
+    // raw.c_cc[VTIME] = 10; // 10 * 100 msec = 10s timeout
 
     // tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
@@ -65,6 +71,10 @@ void enableRawMode() {
     }
 }
 
+/**
+ * @brief Terminal API
+ * @details Disable Raw Input
+ */
 void disableRawMode() {
   // tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 
@@ -75,7 +85,43 @@ void disableRawMode() {
 
 }
 
+/**
+ * @brief Terminal API
+ * @details Error Handler
+ * 
+ * @param s Error Message
+ */
 void terminate(const char *s){
     perror(s);
     exit(1);
+}
+
+/**
+ * @brief Terminal API
+ * @details Read Input
+ * @return Byte char
+ */ 
+char texReadKey(){
+    int nChar;
+    char c;
+    while(nChar = read(STDIN_FILENO, &c, 1) != 1 ){
+        if (nChar == -1 && errno != EAGAIN) // Again, Cygwin compatibility
+        {
+            terminate("read");
+        }
+    }
+    return c;
+}
+
+/**
+ * @brief Input Handling
+ * @details Comprise keystrokes
+ */
+void texProcessKey(){
+    char c = texReadKey();
+    switch(c){
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
 }
