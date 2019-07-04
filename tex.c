445783340c893651ff30068ff30e38d1a0f5dcd2
tinng81 <tinng801@gmail.com>
2019-07-05 02:12:04 +0700

    Step 12
    Timeout for Reading input

--- tex.c
+++ tex.c
@@ -14,14 +14,20 @@ int main(int argc, char const *argv[])
 
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    while(1){
+        char c = "\0";
+        read(STDIN_FILENO, &c, 1) == 1;
+
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
         }
         else {
             printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q')
+        {
+            break;
         }
     }
 
@@ -37,6 +43,9 @@ void enableRawMode() {
   raw.c_oflag &= ~(OPOST); 
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
   raw.c_cflag |= (CS8);
+
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 10; // 10 * 100 msec = 10s timeout
   
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
