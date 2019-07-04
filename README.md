97d552e9641224676d84957274b929864bbf0fd0
tinng81 <tinng801@gmail.com>
2019-07-04 23:32:44 +0700

    Step 4
    Restore original terminal attributes at exit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TEx
+A "1000-liner" text editor within macOS Terminal (Zsh) using purely C and built-in libraries.
+
+# Notice
+This project is under construction !
--- tex.c
+++ tex.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
 #include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
 
 void enableRawMode();
+void disableRawMode();
 
 int main(int argc, char const *argv[])
 {
@@ -15,8 +19,15 @@ int main(int argc, char const *argv[])
 }
 
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
+  
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
